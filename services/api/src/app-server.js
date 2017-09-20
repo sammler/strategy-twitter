537a744f196c37426670e586b4f8350cf351c0d6
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-09-21 01:26:09 +0200

    feat: listen to the heartbeat

--- services/api/src/app-server.js
+++ services/api/src/app-server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const amqpSugar = require('./lib/amqplib-sugar');
 const logger = require('winster').instance();
 const MongooseConnection = require('mongoose-connection-promise');
 const _ = require('lodash');
 const routesConfig = require('./config/routes-config');
-const configDefault = require('./config/config-default');
+
+// Todo: has to be removed ...
+const RABBITMQ_URI = process.env.SAMMLER_RABBITMQ_URI || 'amqp://guest:guest@localhost:5672';
+
+
+// Todo: This is a big construction area, needs to be resolved ... ?? in a general repo handling that?
+const configDefault = require('./config/config');
 
 class AppServer {
   constructor(config) {
@@ -13,6 +20,7 @@ class AppServer {
     // console.log('config:after', this.config);
     this.server = null;
     this.logger = logger;
+    // this.logger.info('mongoose connection', this.config.MONGOOSE_CONNECTION);
     this.mongooseConnection = new MongooseConnection(this.config.MONGOOSE_CONNECTION || MongooseConnection.DEFAULT_OPTIONS);
     this._initApp();
   }
@@ -22,6 +30,24 @@ class AppServer {
     this.app.use(bodyParser.json());
 
     routesConfig.init(this.app);
+  }
+
+  /**
+   * Initialize the RabbitMQ subscribers ...
+   * @private
+   */
+  _initSubscribers() {
+
+    const opts = {
+      server: RABBITMQ_URI,
+      exchange: {
+        type: 'topic',
+        name: 'system'
+      },
+      key: 'heartbeat'
+    };
+
+    amqpSugar.subscribeMessage(opts);
   }
 
   start() {
@@ -36,10 +62,15 @@ class AppServer {
               return reject(err);
             }
             this.logger.info(`Express server listening on port ${this.config.PORT} in "${process.env.NODE_ENV}" mode`);
+            console.log(`Express server listening on port ${this.config.PORT} in "${process.env.NODE_ENV}" mode`);
+            this._initSubscribers();
             return resolve();
           });
         })
-        .catch(err => reject(err));
+        .catch(err => {
+          this.logger.fatal('An error happened connecting to MongoDB', err);
+          reject(err)
+        });
     });
   }
 
--- services/api/src/config/winster-config.js
+++ services/api/src/config/winster-config.js
@@ -1,1 +1,4 @@
-module.exports = require('sammler-nodelib-logger').WINSTER_CONFIG;
+// module.exports = require('sammler-nodelib-logger').WINSTER_CONFIG;
+// console.log('config', config);
+// Todo: Fix fetching the configuration here, does not seem to work ...
+module.exports = {};
